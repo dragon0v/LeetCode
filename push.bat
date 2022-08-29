058b82e08e0de33aa81f293b6d77bb616270776c
dragon0v <2307022423@qq.com>
2022-08-29 14:32:10 +0800

    testtt

--- push.bat
+++ push.bat
@@ -2,6 +2,7 @@
 @REM git remote add origin git@github.com:dragon0v/LeetCode.git
 @REM git config credential.helper store
 git add .
-git commit -m "test"
+git commit -m "testtt"
 git push -u origin
+
 
