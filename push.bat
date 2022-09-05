cace2bcad9b3471418a5c20a735ab3def889937a
dragon0v <2307022423@qq.com>
2022-09-05 21:28:38 +0800

    change this .bat file's format to GBK 测试

--- push.bat
+++ push.bat
@@ -2,7 +2,7 @@
 @REM git remote add origin git@github.com:dragon0v/LeetCode.git
 @REM git config credential.helper store
 git add .
-git commit -m "update 1137"
+git commit -m "change this .bat file's format to GBK ≤‚ ‘"
 git push -u origin
 
 
