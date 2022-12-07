20d60270cfb536491611e363155d2132dc520478
dragon0v <2307022423@qq.com>
2022-12-07 20:17:01 +0800

    娴嬭瘯涓枃涔辩爜闂

--- push.bat
+++ push.bat
@@ -1,9 +1,10 @@
 @REM git init
 @REM git remote add origin git@github.com:dragon0v/LeetCode.git
 @REM git config credential.helper store
+@REM git config --global core.quotepath false --中文编码
 git add .
-@REM 中文会乱码, use English
-git commit -m "update 63"
-git push -u origin
+@REM commit信息尽量 use English
+git commit -m "测试中文乱码问题"
+@REM git push -u origin
 
 
